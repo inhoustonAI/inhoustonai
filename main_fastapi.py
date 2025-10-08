22e8dcfdc62de8e68f3ff0543b1e6c3914b66e05
Sundin Galué <info@inhoustontexas.us>
2025-10-08 15:31:54 -0500

    Todo

--- main_fastapi.py
+++ main_fastapi.py
@@ -44,10 +44,13 @@ async def twiml_webhook(_: Request):
     xml = """<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Connect>
-    <Stream url="wss://inhouston-ai-api.onrender.com/media" />
+    <Stream url="wss://inhouston-ai-api.onrender.com/media">
+      <Parameter name="track" value="inbound_track"/>
+    </Stream>
   </Connect>
 </Response>"""
     return Response(content=xml.strip(), media_type="application/xml")
+
 
 # ==============================
 # HELPERS
